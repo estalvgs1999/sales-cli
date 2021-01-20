de2e6f589ed4b54fe15f4866aee797b2723f430c
esteban alvarado <estalvgs1999@gmail.com>
2021-01-20 17:44:23 -0600

    feat: :sparkles: welcome interactive menu

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+
 clients = 'pablo,ricardo,'
 
+def create_client(client_name):
+    global clients
+    clients += f'{client_name},'
+
+
+def list_clients():
+    global clients
+    for client in clients.split(','):
+        print(f'- {client}')
+
+def _print_welcome():
+    print('WELCOME TO SALES CLI')
+    print('*'*50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
+
 if __name__ == "__main__":
-    clients += 'david'
-    print(clients)
+    _print_welcome()
+    command = input()
+
+    if command.upper() == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
