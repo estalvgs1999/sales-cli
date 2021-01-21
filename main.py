95ebc7ace26e3b892efa0aac53bc1c9e46c05322
esteban alvarado <estalvgs1999@gmail.com>
2021-01-20 18:40:04 -0600

    feat: :sparkles: search client method

--- main.py
+++ main.py
@@ -33,13 +33,25 @@ def delete_client(client_name):
     else:
         print('Client is not in client\'s list')
 
+
+def search_client(client_name):
+    clients_list = clients.split(',') 
+
+    for client in clients_list:
+        if client == client_name:
+            return True
+    return False
+
+
 def _print_welcome():
     print('WELCOME TO SALES CLI')
     print('*'*50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[L]ist clients')
     print('[D]elete client')
-    print('[U]pdate')
+    print('[U]pdate client')
+    print('[S]earch client')
 
 
 def _get_client_name():
@@ -58,10 +70,19 @@ if __name__ == "__main__":
         client_name = _get_client_name()
         delete_client(client_name)
         list_clients()
+    elif command == 'L':
+        list_clients()
     elif command == 'U':
         client_name = _get_client_name()
         updated_name = input('What is the updated client name? ')
         update_client(client_name, updated_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print(f'The client: {client_name} is not in our client\'s list')
     else:
         print('Invalid command')
