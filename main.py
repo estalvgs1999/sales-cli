4ec14f1f31c776f3c63e0a775379cd9485842b95
esteban alvarado <estalvgs1999@gmail.com>
2021-01-20 19:10:14 -0600

    refactor: :recycle: implementation of lists intead of strings

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+import sys
 
-clients = 'pablo,ricardo,'
+clients = ['Pablo','Ricardo']
 
 def create_client(client_name):
     global clients
 
     if client_name not in clients:
-        clients += f'{client_name},'
+        clients.append(client_name)
     else:
         print('Client already in the client\'s list')
 
 
 def list_clients():
     global clients
-    for client in clients.split(','):
-        print(f'- {client}')
+    
+    for idx,client in enumerate(clients):
+        print(f'{idx}: {client}')
 
 
 def update_client(client_name, updated_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name, updated_name)
+        index = clients.index(client_name)
+        clients[index] = updated_name
     else:
         print('Client is not in client\'s list')
 
@@ -29,17 +32,16 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(f'{client_name},','')
+        clients.remove(client_name)
     else:
         print('Client is not in client\'s list')
 
 
 def search_client(client_name):
-    clients_list = clients.split(',') 
-
-    for client in clients_list:
+    for client in clients:
         if client == client_name:
             return True
+    
     return False
 
 
@@ -55,8 +57,15 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
 
+    while not client_name:
+        client_name = input('What is the client name? ').strip()
+
+        if client_name == 'exit':
+            sys.exit()
+        
+    return client_name
 
 if __name__ == "__main__":
     _print_welcome()
@@ -86,3 +95,4 @@ if __name__ == "__main__":
             print(f'The client: {client_name} is not in our client\'s list')
     else:
         print('Invalid command')
+        
